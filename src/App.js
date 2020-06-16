c6b04fe8f18abc9707756925c74afad23b536d65
Anant Prasad <anant29october@gmail.com>
2020-06-17 02:01:06 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
 import Error from './pages/Error';
-
+import Navbar from './components/Navbar'
 export default function App() {
   return <Router>
+    <Navbar />
     <Switch>
       <Route exact path="/"><Home /></Route>
       <Route exact path="/about"><About /></Route>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,14 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
 export default function Navbar() {
-  return <h1>navbar component</h1>;
+  return <nav className="navbar">
+    <div className="nav-center">
+      <img src={logo} alt="cocktail db logo" className="logo" />
+      <ul className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
+    </div>
+  </nav>;
 }

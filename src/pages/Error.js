6bfdd0ad7964474041390b0337072076dc447f44
Anant Prasad <anant29october@gmail.com>
2020-06-17 02:05:29 +0530

    error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,11 @@
 import React from "react";
+import {Link } from 'react-router-dom'
 
 export default function Error() {
-  return <h1>error page</h1>;
+  return <section className="error-page section">
+    <div className="error-container">
+      <h1>OOPS! IT's A DEAD END</h1>
+      <Link to="/" className="btn btn-primary">Back to Home</Link>
+    </div>
+  </section>;
 }

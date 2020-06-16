71bd2a75041abb0e26c3681213952b0a0a332aee
Anant Prasad <anant29october@gmail.com>
2020-06-17 02:03:23 +0530

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function About() {
-  return <h1>about page</h1>;
+  return <section className="section about-section">
+    <h1 className="section-title">about us</h1>
+    <p> 'm baby put a bird on it intelligentsia waistcoat enamel pin, post-ironic flexitarian neutra aesthetic PBR&B etsy. Vexillologist slow-carb mlkshk chia. Kinfolk ramps normcore fixie you probably haven't heard of them prism cred. Af portland bicycle rights cliche. Activated charcoal franzen mlkshk, food truck four loko subway tile pork belly banh mi yuccie authentic organic actually four dollar toast. Tote bag polaroid typewriter, fixie affogato vinyl locavore. Mumblecore seitan keffiyeh, vape artisan organic williamsburg deep v photo booth wolf.
+
+    </p>
+  </section>;
 }
